a280676452b34deddd1a9d8d04d1f776dbb0cf08
Maycon Pires <maycon.pires@inpe.br>
2026-02-19 16:50:39 -0300

    refc proj

--- rpgforge/src/app/auth/verify-request/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function VerifyRequest() {
-  return (
-    <main style={{ maxWidth: 420, margin: "48px auto" }}>
-      <h1>Verifique seu e-mail</h1>
-      <p>Enviamos um link de acesso para o seu e-mail.</p>
-    </main>
-  )
-}
